7be75376b63f2c960365148a62eff2e835443046
derojp <joachim.dero@mow.vlaanderen.be>
2025-11-26 19:48:23 +0100

    add modulesFromGithub

--- toolboxLocatieservices2/Ls2AttributenEindpunten.py
+++ toolboxLocatieservices2/Ls2AttributenEindpunten.py
@@ -1,3 +1,31 @@
+import json
 import os
-def test():
-    pass
+import sys
+import urllib
+
+
+def load_module_from_github(modules=modulesFromGithub.json, feedback=None):
+    with open("modulesFromGithub.json") as f:
+        modules = json.load(f)
+
+    for name, url in modules.items():
+        mod = load_module_from_github(url, name)
+        feedback.pushInfo(f"Loaded: {name} -> {dir(mod)}")
+
+    cache_dir = os.path.join(os.path.expanduser("~"), ".qgis_module_cache")
+    os.makedirs(cache_dir, exist_ok=True)
+
+    local_path = os.path.join(cache_dir, module_name + ".py")
+    urllib.request.urlretrieve(url, local_path)
+
+    sys.path.append(cache_dir)
+    module = __import__(module_name)
+    feedback.pushInfo(f"module:{module}")
+    return module
+
+
+
+def main(parameters, feedback=None):
+
+    raw_url = "https://raw.githubusercontent.com/joachimdero/AwvFuncties_os/refs/heads/master/libs/AuthenticatieProxyAcmAwv.py?"
+    Auth = load_module_from_github(raw_url, "AuthenticatieProxyAcmAwv", feedback=None)
