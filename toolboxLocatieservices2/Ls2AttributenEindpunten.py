135d405bfeaf32b70d0a6e46171d1eb86cc9a050
derojp <joachim.dero@mow.vlaanderen.be>
2025-11-26 20:32:13 +0100

    add modulesFromGithub

--- toolboxLocatieservices2/Ls2AttributenEindpunten.py
+++ toolboxLocatieservices2/Ls2AttributenEindpunten.py
@@ -46,5 +46,7 @@ def load_module_from_github(feedback=None):
 def main(parameters, feedback=None):
     loaded_modules = load_module_from_github()
     feedback.pushInfo(str(loaded_modules))
+    feedback.pushInfo(str(dir(loaded_modules["Ls2AttributenEindpunten"])))
+    feedback.pushInfo(str(dir(AuthenticatieProxyAcmAwvAuthenticatieProxyAcmAwv)))
 
 
