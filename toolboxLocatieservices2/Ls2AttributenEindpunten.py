622569688d3cf1826196f229cfa9ceab12d2e83b
derojp <joachim.dero@mow.vlaanderen.be>
2025-11-28 23:53:33 +0100

    add modulesFromGithub

--- toolboxLocatieservices2/Ls2AttributenEindpunten.py
+++ toolboxLocatieservices2/Ls2AttributenEindpunten.py
@@ -41,6 +41,7 @@ def load_module_from_github(feedback=None):
             if feedback:
                 feedback.reportError(f"Fout bij importeren {module_name}: {e}", fatalError=False)
         import AuthenticatieProxyAcmAwv
+        import Locatieservices2
 
     return loaded_modules
 
